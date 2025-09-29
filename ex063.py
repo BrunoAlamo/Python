4b4d7ff77531e96cb340d6768c2113816473c01e
BrunoAlamo <147200493+BrunoAlamo@users.noreply.github.com>
2025-09-29 12:39:00 -0300

    Add files via upload

--- ex063.py
+++ ex063.py
@@ -1,11 +1,10 @@
-n = int(input('quantos termos da sequencia de Fibonacci voce quer mostrar? '))
+n = int(input('digite quantos termos da sequencia de Fibonacci vc quer mostrar: '))
 
 t1 = 0
 t2 = 1
 t3 = 1
 f = [t1, t2, t3]
 r = 0
-
 
 for c in range(1, n - 2):
     f.append(f[c] + f[c + 1])
@@ -14,6 +13,3 @@ while n != 0:
     print(f[r], end=' ')
     r += 1
     n -= 1
-
-
-
