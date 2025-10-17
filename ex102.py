a2f96cd3772b65fd2d931c93b16186443e2e68bc
BrunoAlamo <147200493+BrunoAlamo@users.noreply.github.com>
2025-10-16 23:57:48 -0300

    Add files via upload

--- ex102.py
+++ ex102.py
@@ -1,14 +1,28 @@
+from ctypes import c_char
+
+
 def fatorial(n, show=False):
-    c = 1
-    f = 0
-    print(f'{n}', end='')
-    while c != n:
-        print(f' x {n - c}', end='')
-        f += n * (n - c)
-        c += 1
-    print(' = ', end='')
-    print(f)
+    c = n
+    f = 1
+    if show==True:
+        print(f'{n}', end='')
+        while c > 0:
+            if c > 1:
+                print(f' x {c - 1}', end='')
+                f *= c
+                c -= 1
+            else:
+                print(f' {c}', end='')
+                break
+        print(' = ', end='')
+        print(f)
+    else:
+        while c > 0:
+            f *= c
+            c -= 1
+        print(f)
 
 
 
 fatorial(5)
+fatorial(6, True)
