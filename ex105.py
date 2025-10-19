6d8994da55e13e628c817dc99946d41a133b6df3
BrunoAlamo <147200493+BrunoAlamo@users.noreply.github.com>
2025-10-19 20:20:01 -0300

    Add files via upload

--- ex105.py
+++ ex105.py
@@ -1,11 +1,16 @@
-def notas(* num):
+def notas(* num, sit=False):
     aluno = {}
     aluno['total'] = len(num)
     aluno['maior'] = max(num)
     aluno['menor'] = min(num)
     aluno['media'] = sum(num) / len(num)
+    if sit==True:
+        if aluno['media'] >= 7:
+            aluno['sit'] = 'BOA'
+        else:
+            aluno['sit'] = 'RUIM'
     return aluno
 
 #main
-resp = notas(5.5, 9.5, 10, 6.5)
+resp = notas(5.5, 9.5, 10, 6.5, sit=True)
 print(resp)
